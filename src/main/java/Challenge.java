953cdddb938c86778ef28dc593ab7ba4c001c1e1
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-29 21:48:26 -0500

    feat: warOfNumbers method completed

--- src/main/java/Challenge.java
+++ src/main/java/Challenge.java
@@ -24,4 +24,40 @@ public class Challenge {
             return 2;
         }
     }
+
+    // War of Numbers
+    // There's a great war between the even and odd numbers. Many numbers already lost their life in this war and it's your task to end this. You have to determine which group sums larger: the even, or the odd. The larger group wins.
+    //
+    //Create a function that takes an array of integers, sums the even and odd numbers separately, then returns the difference between sum of even and odd numbers.
+    //
+    //Examples
+    //warOfNumbers([2, 8, 7, 5]) ➞ 2
+    //// 2 + 8 = 10
+    //// 7 + 5 = 12
+    //// 12 is larger than 10
+    //// So we return 12 - 10 = 2
+    //
+    //warOfNumbers([12, 90, 75]) ➞ 27
+    //
+    //warOfNumbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]) ➞ 168
+    public static int warOfNumbers(int[] ints) {
+        int sumOfEven = 0;
+        int sumOfOdd = 0;
+
+        for (int number : ints) {
+            if (number % 2 == 0) {
+                sumOfEven += number;
+            } else {
+                sumOfOdd += number;
+            }
+        }
+
+        if (sumOfEven >= sumOfOdd) {
+            return sumOfEven - sumOfOdd;
+        } else {
+            return sumOfOdd - sumOfEven;
+        }
+    }
 }
+
+
--- src/test/java/ChallengeTest.java
+++ src/test/java/ChallengeTest.java
@@ -6,37 +6,29 @@ public class ChallengeTest {
 
 //    How Many Solutions Does This Quadratic Have?
     @Test
-    public void test1() {
+    public void testIfSolutionsWorks() {
         assertEquals(2, Challenge.solutions(1, 0, -1));
-    }
-
-    @Test
-    public void test2() {
         assertEquals(1, Challenge.solutions(1, 0, 0));
-    }
-
-    @Test
-    public void test3() {
         assertEquals(0, Challenge.solutions(1, 0, 1));
-    }
-
-    @Test
-    public void test4() {
         assertEquals(0, Challenge.solutions(200, 420, 800));
-    }
-
-    @Test
-    public void test5() {
         assertEquals(2, Challenge.solutions(200, 420, -800));
-    }
-
-    @Test
-    public void test6() {
         assertEquals(2, Challenge.solutions(1000, 1000, 0));
-    }
-
-    @Test
-    public void test7() {
         assertEquals(1, Challenge.solutions(10000, 400, 4));
     }
+
+
+    // War of Numbers
+    @Test
+    public void testIfWareOfNumbersWorks() {
+        assertEquals(168, Challenge.warOfNumbers(new int[]{5,9,45,6,2,7,34,8,6,90,5,243}));
+        assertEquals(793, Challenge.warOfNumbers(new int[]{654,7,23,3,78,4,56,34}));
+        assertEquals(5, Challenge.warOfNumbers(new int[]{1,2,3,4,5,6,7,8,9}));
+        assertEquals(228, Challenge.warOfNumbers(new int[]{97,83,209,141,134,298,110,207,229,275,115,64,244,278}));
+        assertEquals(83, Challenge.warOfNumbers(new int[]{332,92,35,315,109,168,320,230,63,323,16,204,105,17,226,157,245,44,223,136,93}));
+        assertEquals(846, Challenge.warOfNumbers(new int[]{322,89,36,310,297,157,251,55,264,244,200,304,25,308,311,269,303,257,6,311,307,310,50,46,54,237,59,105,267}));
+        assertEquals(0, Challenge.warOfNumbers(new int[]{50,100,149,1,200,199,3,2}));
+    }
+
+
+
 }
