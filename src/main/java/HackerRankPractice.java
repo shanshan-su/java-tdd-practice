221a4b186e1d98a2e6fb453dee801a3caeb1ef1a
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-08-04 21:46:35 -0500

    feat: added if-else practice

---
+++ src/main/java/HackerRankPractice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HackerRankPractice {
+
+    public  static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.next());
+        if (n % 2 == 1) {
+            System.out.println("Weird");
+        } else if (n >= 2 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if  (n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+    }
+}
