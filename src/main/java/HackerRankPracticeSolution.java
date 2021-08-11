02f82eba50ddc63a3cd0f38a257d336892f01226
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-08-10 22:07:59 -0500

    feat: added Java Currency Formatter

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class HackerRankPracticeSolution {
@@ -437,7 +438,56 @@ public class HackerRankPracticeSolution {
         bufferedReader.close();
         bufferedWriter.close();
 
+        // Given a double-precision number, , denoting an amount of money, use the NumberFormat class' getCurrencyInstance method to convert  into the US, Indian, Chinese, and French currency formats. Then print the formatted values as follows:
+        //
+        //US: formattedPayment
+        //India: formattedPayment
+        //China: formattedPayment
+        //France: formattedPayment
+        //where  is  formatted according to the appropriate Locale's currency.
+        //
+        //Note: India does not have a built-in Locale, so you must construct one where the language is en (i.e., English).
+        //
+        //Input Format
+        //
+        //A single double-precision number denoting .
+        //
+        //Constraints
+        //
+        //Output Format
+        //
+        //On the first line, print US: u where  is  formatted for US currency.
+        //On the second line, print India: i where  is  formatted for Indian currency.
+        //On the third line, print China: c where  is  formatted for Chinese currency.
+        //On the fourth line, print France: f, where  is  formatted for French currency.
+        //
+        //Sample Input
+        //
+        //12324.134
+        //Sample Output
+        //
+        //US: $12,324.13
+        //India: Rs.12,324.13
+        //China: ￥12,324.13
+        //France: 12 324,13 €
+        //Explanation
+        //
+        //Each line contains the value of  formatted according to the four countries' respective currencies.
 
+        double payment = scanner.nextDouble();
+
+        NumberFormat numberFormat;
+        numberFormat = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
+        System.out.printf("US: %s%n", numberFormat.format(payment));
+
+        numberFormat = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        System.out.printf("India: %s%n", numberFormat.format(payment));
+
+        numberFormat = NumberFormat.getCurrencyInstance(new Locale("zh", "CN"));
+        System.out.printf("China: %s%n", numberFormat.format(payment));
+
+        numberFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.printf("France: %s%n", numberFormat.format(payment));
 
 
 
