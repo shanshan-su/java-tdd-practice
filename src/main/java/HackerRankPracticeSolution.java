3315133cd66f3f74cb962432fca4f305b3d0b55f
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-08-30 15:17:54 -0500

    feat: completed Java MD5

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -1,9 +1,11 @@
 import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.NumberFormat;
 import java.util.*;
 
 public class HackerRankPracticeSolution {
-    public  static void main(String[] args) throws IOException {
+    public  static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Scanner scanner = new Scanner(System.in);
 
         // Given an integer n, perform the following conditional actions:
@@ -1054,6 +1056,53 @@ public class HackerRankPracticeSolution {
                 System.out.println(letter);
             }
         }
+
+        // MD5 (Message-Digest algorithm 5) is a widely-used cryptographic hash function with a 128-bit hash value. Here are some common uses for MD5:
+        //
+        //To store a one-way hash of a password.
+        //To provide some assurance that a transferred file has arrived intact.
+        //MD5 is one in a series of message digest algorithms designed by Professor Ronald Rivest of MIT (Rivest, 1994); however, the security of MD5 has been severely compromised, most infamously by the Flame malware in 2012. The CMU Software Engineering Institute essentially considers MD5 to be "cryptographically broken and unsuitable for further use".
+        //
+        //Given an alphanumeric string, s, denoting a password, compute and print its MD5 encryption value.
+        //
+        //Input Format
+        //
+        //A single alphanumeric string denoting s.
+        //
+        //Constraints
+        //
+        // 6 <= |s| <= 20
+        //String s consists of English alphabetic letters (i.e., [a-zA-Z] and/or decimal digits (i.e., 0 through 9) only.
+        //Output Format
+        //
+        //Print the MD5 encryption value of s on a new line.
+        //
+        //Sample Input 0
+        //
+        //HelloWorld
+        //Sample Output 0
+        //
+        //68e109f0f40ca72a15e05cc22786f8e6
+        //Sample Input 1
+        //
+        //Javarmi123
+        //Sample Output 1
+        //
+        //2da2d1e0ce7b4951a858ed2d547ef485
+
+        string = scanner.nextLine();
+
+        /* Encode the String using MD5 */
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(string.getBytes());
+        byte[] digest = md.digest();
+
+        /* Print the encoded value in hexadecimal */
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+
+
 
 
 
