070566d6931e63ca352197a3b601e478b11a5642
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-04 14:04:15 -0500

    feat: added day 7 of 30 Days of Code

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -6,6 +6,9 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class HackerRankPracticeSolution {
     public  static void main(String[] args) throws IOException, NoSuchAlgorithmException {
@@ -1396,9 +1399,49 @@ public class HackerRankPracticeSolution {
             System.out.println(evenString + " " + oddString);
         }
 
+        // Task
+        //Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+        //
+        //Example
+        //
+        // A = [1, 2, 3, 4]
+        //Print 4 3 2 1. Each integer is separated by one space.
+        //
+        //Input Format
+        //
+        //The first line contains an integer, N(the size of our array).
+        //The second line contains N space-separated integers that describe array A's elements.
+        //
+        //Constraints
+        //
+        // 1 <= N <= 1000
+        // 1 <= A[i] <= 10000, where A[i] is the i integer in the array.
+        //Output Format
+        //
+        //Print the elements of array A in reverse order as a single line of space-separated numbers.
+        //
+        //Sample Input
+        //
+        //4
+        //1 4 3 2
+        //Sample Output
+        //
+        //2 3 4 1
 
+        n = Integer.parseInt(bufferedReader.readLine().trim());
 
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
+        string = "";
+        for (i = arr.size() - 1; i >= 0; i--) {
+            string += arr.get(i) + " ";
+        }
+
+        System.out.println(string.trim());
+
+        bufferedReader.close();
 
 
 
