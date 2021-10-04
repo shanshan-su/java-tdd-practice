48d920accce1f5ee1c4b8c088ee705f42f456c4b
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-03 21:20:20 -0500

    feat: added day 6 of 30 Days of Code

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -1345,6 +1345,58 @@ public class HackerRankPracticeSolution {
 
         in.close();
 
+        // Task
+        //Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+        //
+        //Note: 0 is considered to be an even index.
+        //
+        //Example
+        //
+        // S = adbecf
+        //Print abc def
+        //
+        //Input Format
+        //
+        //The first line contains an integer, T(the number of test cases).
+        //Each line I of the T subsequent lines contain a string, S.
+        //
+        //Constraints
+        // 1<= T <= 10
+        // 2<= length of S <= 10000
+        //Output Format
+        //
+        //For each String Sj (where 0 <= j <= T-1), print Sj's even-indexed characters, followed by a space, followed by Sj's odd-indexed characters.
+        //
+        //Sample Input
+        //
+        //2
+        //Hacker
+        //Rank
+        //Sample Output
+        //
+        //Hce akr
+        //Rn ak
+
+        number = scanner.nextInt();
+        scanner.nextLine();
+
+        for (i = 1; i <= number; i++) {
+            string = scanner.nextLine();
+
+            String evenString = "";
+            String oddString = "";
+
+            for (int j = 0; j < string.length(); j++) {
+                if (j % 2 == 0) {
+                    evenString += Character.toString(string.charAt(j));
+                } else {
+                    oddString += Character.toString(string.charAt(j));
+                }
+            }
+            System.out.println(evenString + " " + oddString);
+        }
+
+
 
 
 
