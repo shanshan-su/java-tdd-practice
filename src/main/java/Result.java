fe8ff356cd821783aafceaf0d0f413480137f846
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-06 21:10:58 -0500

    feat: added day 9 of 30 Days of Code

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 public class Result {
@@ -18,5 +19,65 @@ public class Result {
 
         int p = c.get(Calendar.DAY_OF_WEEK);
         return days.get(p - 1);
+    }
+
+    // Function Description
+    //Complete the factorial function in the editor below. Be sure to use recursion.
+    //
+    //factorial has the following paramter:
+    //
+    //int n: an integer
+    //Returns
+    //
+    //int: the factorial of n
+    //Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of 0.
+    //
+    //Input Format
+    //
+    //A single integer, n(the argument to pass to factorial).
+    //
+    //Constraints
+    //
+    // 2 < = n <= 12
+    //Your submission must contain a recursive function named factorial.
+    //Sample Input
+    //
+    //3
+    //Sample Output
+    //
+    //6
+    //Explanation
+    //
+    //Consider the following steps. After the recursive calls from step 1 to 3, results are accumulated from step 3 to 1.
+    // factorial(3) = 3 x factorial(2) = 3 x 2 = 6
+    // factorial(2) = 2 x factorial(1) = 2 x 1 = 2
+    // factorial(1) = 1
+
+    public static int factorial(int n) {
+        // Write your code here
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
