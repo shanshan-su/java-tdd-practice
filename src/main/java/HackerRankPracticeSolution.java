a4c6e2ab21eea5e39caf5ecc95f6dd39f7b2dab3
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-18 20:25:01 -0500

    feat: added day 21 of 30 Days of Code

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -2189,6 +2189,42 @@ public class HackerRankPracticeSolution {
         System.out.println("Last Element: " + numbers.get(n - 1));
         bufferedReader.close();
 
+        // Task
+        //Write a single generic function named printArray; this function must take an array of generic elements as a parameter (the exception to this is C++, which takes a vector). The locked Solution class in your editor tests your function.
+        //
+        //Note: You must use generics to solve this challenge. Do not write overloaded functions.
+        //
+        //Input Format
+        //
+        //The locked Solution class in your editor will pass different types of arrays to your printArray function.
+        //
+        //Constraints
+        //
+        //You must have exactly  function named printArray.
+        //Output Format
+        //
+        //Your printArray function should print each element of its generic array parameter on a new line.
+        n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+
+
     }
 
 
