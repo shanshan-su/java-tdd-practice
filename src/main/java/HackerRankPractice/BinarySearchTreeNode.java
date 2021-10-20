fcad7845cee3d32c6db4f755296bf41ab3e0c8a6
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-19 23:07:07 -0500

    feat: added day 22 of 30 Days of Code

---
+++ src/main/java/HackerRankPractice/BinarySearchTreeNode.java
@@ -0,0 +1,12 @@
+package HackerRankPractice;
+
+public class BinarySearchTreeNode {
+    public BinarySearchTreeNode left;
+    public BinarySearchTreeNode right;
+    public int data;
+
+    public BinarySearchTreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
