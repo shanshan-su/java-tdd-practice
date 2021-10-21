55fe11804e12296fb89e795aafe899fb85419ac8
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-10-20 21:42:35 -0500

    feat: added day 23 od 30 Days of Code

--- src/main/java/HackerRankPracticeSolution.java
+++ src/main/java/HackerRankPracticeSolution.java
@@ -2226,13 +2226,65 @@ public class HackerRankPracticeSolution {
 
 
         T = scanner.nextInt();
-        BinarySearchTreeNode root=null;
+        BinarySearchTreeNode root = null;
         while(T-->0){
             int data = scanner.nextInt();
             root = insert(root,data);
         }
         int height=getHeight(root);
         System.out.println(height);
+
+        // Day 23 - Task
+        //A level-order traversal, also known as a breadth-first search, visits each level of a tree's nodes from left to right, top to bottom. You are given a pointer, root, pointing to the root of a binary search tree. Complete the levelOrder function provided in your editor so that it prints the level-order traversal of the binary search tree.
+        //
+        //Hint: You'll find a queue helpful in completing this challenge.
+        //
+        //Function Description
+        //
+        //Complete the levelOrder function in the editor below.
+        //
+        //levelOrder has the following parameter:
+        //- Node pointer root: a reference to the root of the tree
+        //
+        //Prints
+        //- Print node.data items as space-separated line of integers. No return value is expected.
+        //
+        //Input Format
+        //
+        //The locked stub code in your editor reads the following inputs and assembles them into a BST:
+        //The first line contains an integer, T(the number of test cases).
+        //The T subsequent lines each contain an integer, data, denoting the value of an element that must be added to the BST.
+        //
+        //Constraints
+        //
+        // 1 <= N <= 20
+        // 1 <= node.data[i] <= 100
+        //Output Format
+        //
+        //Print the data value of each node in the tree's level-order traversal as a single line of N space-separated integers.
+        //
+        //Sample Input
+        //
+        //6
+        //3
+        //5
+        //4
+        //7
+        //2
+        //1
+        //Sample Output
+        //
+        //3 2 5 1 4 7
+
+        T = scanner.nextInt();
+        root = null;
+        while(T-- > 0){
+            int data=scanner.nextInt();
+            root = insert(root,data);
+        }
+        levelOrder(root);
+
+
 
     }
 
@@ -2278,7 +2330,7 @@ public class HackerRankPracticeSolution {
             return new BinarySearchTreeNode(data);
         }
         else{
-            Node cur;
+            BinarySearchTreeNode cur;
             if(data<=root.data){
                 cur=insert(root.left,data);
                 root.left=cur;
@@ -2288,6 +2340,29 @@ public class HackerRankPracticeSolution {
                 root.right=cur;
             }
             return root;
+        }
+    }
+
+    // day 23: BST Level-Order Traversal
+    public static void levelOrder(BinarySearchTreeNode root){
+        //Write your code here
+        Queue<BinarySearchTreeNode> queue = new LinkedList();
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while(!queue.isEmpty()) {
+            // get the current element in queue and then print out its data
+            BinarySearchTreeNode current = queue.remove();
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
         }
     }
 }
