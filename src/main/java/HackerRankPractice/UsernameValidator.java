60c6123379a431443a85954340406d16ca3061d1
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-11-01 13:42:15 -0500

    feat: added Valid Username Regular Expression exercise

---
+++ src/main/java/HackerRankPractice/UsernameValidator.java
@@ -0,0 +1,9 @@
+package HackerRankPractice;
+
+public class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "[a-zA-Z](\\w){7,29}";
+    // public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+}
